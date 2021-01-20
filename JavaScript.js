3915b1052096e8bbefe6449eb87ea6710d80af0b
MaXOlimp <kirichukm@gmail.com>
2021-01-20 22:09:51 +0100

    test

---
+++ JavaScript.js
@@ -0,0 +1,33 @@
+//Zadanie 1 (Hello JavaScript)
+alert('«Я JavaScript!»');
+
+alert("Я JavaScript!");
+
+
+// Zadanie 2 (variables)
+
+let name = "Jon";
+let admin;
+admin = name;
+
+alert(admin);
+console.log(admin)
+
+//Zadanie 3 (Data types)  co bd pokazywac ten alert?
+
+let name2 = "Ilya";
+
+alert(`hello ${1}`); //  hello 1
+
+alert(`hello ${"name2"}`); // hello name2
+
+alert(`hello ${name2}`); // hello Ilya
+
+//Zadanie 4 (alert, prompt, confirm) 
+
+let howYourName = prompt("How your name");
+alert(`Your name ${howYourName}`)
+
+//Zadanie 5 ()
+
+alert("Heelooo Worlldd")
