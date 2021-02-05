15472a4eb4748c32e16a69574fdf36ff07ba327f
MaXOlimP <kirichukm@gmail.com>
2021-02-05 11:44:38 +0100

    create a new

--- JavaScript.js
+++ JavaScript.js
@@ -110,3 +110,38 @@ function zadanie7() {
             (login == "") ? alert("NieMasz") :
                 alert("");
 }
+
+//Zadanie 8(|| && !)
+
+function zadanie8() {
+
+    let age = prompt("number?");
+    if (age >= 14 && age <= 90 && alert("V diapozone"));
+
+
+    let login = prompt("Login?");
+    if (login == "Admin") {
+        let password = prompt("Password?");
+        if (password == "ImBoss") {
+            alert("witam Pana");
+        } else if (pass == " " || pass == null) {
+            alert("cancle ");
+        } else {
+            alert("Uncorect Password!");
+        }
+    } else if (login == " " || login == null) {
+        alert("cancle login");
+    } else {
+        alert("I did know you");
+    }
+
+}
+
+
+
+
+
+
+
+
+
