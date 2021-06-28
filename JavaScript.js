fda8b3fdd2b71a67f6e49e33ec10403bee96d030
MaXOlimP <kirichukm@gmail.com>
2021-06-28 20:56:02 +0200

    add to js last task for logistik operators

--- JavaScript.js
+++ JavaScript.js
@@ -11,7 +11,29 @@ function zadanie1() {
 
 
 
+function test() {
+    let name = prompt('Take a login');
 
+    if (name == 'Admin') {
+
+        let password = prompt('Take a password');
+
+        if (password == 'admin') {
+            alert('You Big Boss!');
+        } else if (password == '' || password == null) {
+            alert('escape');
+        } else {
+            alert('pls true again');
+        }
+
+    } else if (name == '' || name == null) {
+        alert('Otmena');
+    } else {
+        alert("I dont know you!");
+    }
+
+}
+test();
 
 
 
