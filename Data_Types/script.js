87cd9fd0f3cc3d50409a9cc2d3bd00d54e47ead0
MaXOlimP <maxolimp7@gmail.com>
2022-05-05 13:28:22 +0200

    test2

--- Data_Types/script.js
+++ Data_Types/script.js
@@ -1,11 +1,12 @@
-//Zadanie 3 (Data types)  co bd pokazywac ten alert?
+// 3 (Data types)  co bd pokazywac ten alert?
 
-function zadanie3() {
-    let name2 = "Ilya";
+function test3() {
+    let name2 = "Bob";
 
     alert(`hello ${1}`); //  hello 1
 
     alert(`hello ${"name2"}`); // hello name2
 
-    alert(`hello ${name2}`); // hello Ilya
+    alert(`hello ${name2}`); // hello Bob
 }
+
